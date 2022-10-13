053a363ce4de9afba72071049fe27b42d2f5e4b6
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-10-13 15:20:58 +0200

    basic func

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// do later
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages}, not read yet`
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
